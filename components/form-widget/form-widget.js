e8b65d477b17c7dda00d27ea2254eea0eac3d161
roemhildtg <roemhildtg@gmail.com>
2016-03-04 14:57:58 -0600

    Allow each field to be templated.

--- components/form-widget/form-widget.js
+++ components/form-widget/form-widget.js
@@ -2,8 +2,20 @@
 
 import Map from 'can/map/';
 import Component from 'can/component/';
+import Stache from 'can/view/stache/';
 //import './widget.css!';
 import template from './template.stache!';
+
+const TEMPLATES = {
+  text: '<input type="text" class="form-control" id="{{name}}" name="{{name}}" value="{{value}}" />',
+  select: [
+    '<select id="{{name}}" class="form-control" name="{{name}}" value="{{value}}">',
+    '{{#each properties.options}}',
+    '<option value="{{value}}">{{label}}</option>',
+    '{{/each}}',
+    '</select>'
+  ].join('')
+};
 
 export let viewModel = Map.extend({
   define: {
@@ -11,21 +23,42 @@ export let viewModel = Map.extend({
       type: 'boolean',
       value: false
     },
-    formObject: {
-      value: null
-    },
-    templates: {
-      value: {
-        text: '<input type="text" id="{{%key}}" name="{{%key}}" {($value)}="{{%key}}" />',
-        select: [
-          '<select id="{{%key}}" name="{{%key}}" {($value)}="{{%key}}">',
-          '{{#each options}}',
-          '<option value="{{value}}">{{label}}</option>',
-          '{{/each}}',
-          '</select>'
-        ].join('')
-      }
+    formObject: {},
+    fields: {
+      Type: can.List
     }
+  },
+  init: function() {
+    this.createFields();
+  },
+  createFields: function() {
+    var fields = this.attr('fields');
+    if (!fields) {
+      fields = Map.keys(this.attr('formObject'));
+    }
+    var self = this;
+    this.attr('fieldObjects', {});
+    fields.forEach(function(field) {
+      var obj;
+      if (typeof field === 'string') {
+        obj = {
+          name: field,
+          alias: self.formatField(field),
+          template: Stache(TEMPLATES['text']),
+          properties: {},
+          value: self.attr(['formObject', field].join('.'))
+        };
+      } else {
+        obj = {
+          name: field.name,
+          alias: field.alias || self.formatField(field),
+          template: field.template || Stache(TEMPLATES[field.type || 'text']),
+          properties: field.properties || {},
+          value: self.attr(['formObject', field.name].join('.'))
+        };
+      }
+      self.attr(['fieldObjects', field.name || field].join('.'), obj);
+    });
   },
   formSubmit: function(scope, form, event) {
     var data = can.$(form).serializeArray();
@@ -36,16 +69,44 @@ export let viewModel = Map.extend({
         formObject.attr(newData.name, newData.value);
       }
     }
+    console.log(formObject);
     formObject.save();
     this.dispatch('submit', [formObject]);
     //prevent the form from submitting
     return false;
   },
-  cancelForm: function(){
+  cancelForm: function() {
     this.dispatch('cancel')
   },
+  getAttr: function() {
+    //trim off the last argument
+    var args = Array.prototype.slice.call(arguments, 0, arguments.length - 1);
+    //capture the oject and remove it
+    var obj = args[0];
+    args.splice(0, 1);
+    //return the value
+    return obj.attr(args.join('.'));
+  },
+  /**
+   * Determines whether this field should be rendered
+   * @param  {String} fieldName The fieldname
+   * @return {Boolean} True if the field is in the list of fields, false if otherwise
+   */
   renderField: function(fieldName) {
-    return !this.attr('fields') ? fieldName : this.attr('fields').indexOf(fieldName) > -1;
+    var fields = this.attr('fields');
+    return true;
+  },
+  getField: function(fieldName) {
+    return {
+      alias: fieldProps.attr('alias') || fieldName,
+      template: Stache(TEMPLATES[fieldProps.attr('template') || 'text'])
+    };
+  },
+  getTemplate: function(fieldName) {
+    var template = this.attr('fieldTemplates.' + fieldName) || {};
+    var properties = template.properties || {};
+    properties.key = fieldName;
+    return template.template || Stache(TEMPLATES[template.name || 'text']);
   },
   formatField: function(fieldName) {
     fieldName = String(fieldName);
