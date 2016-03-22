931a296cc36e06cc20b8f35e23f7b960d9c416ca
roemhildtg <roemhildtg@gmail.com>
2016-03-22 15:28:02 -0500

    Split config into different files

--- app/crud/config/default/default.js
+++ app/crud/config/default/default.js
@@ -1,29 +1,5 @@
-import Map from 'can/map/';
-import {
-  FlaskConnectFactory
-} from 'models/FlaskModelFactory';
-
-var TaskConnection = FlaskConnectFactory({
-  url: '/api/tasks_task',
-  name: 'tasks_task',
-  map: Map.extend({
-    name: 'test',
-    description: 'test',
-    yes_or_no: false
-  })
-});
+import {Task} from './models/Task';
 
 export let config = {
-  models: [{
-    title: 'Task',
-    connection: TaskConnection,
-    parameters: {
-      'page[size]': 100
-    },
-    tableFields: ['name', 'description'],
-    editFields: ['name', 'description'],
-    detailFields: ['name', 'description', 'date_created', 'date_modified', 'id'],
-  }]
+  models: [Task]
 };
-
-console.log(TaskConnection);
---
+++ app/crud/config/default/models/Task.js
@@ -0,0 +1,16 @@
+import {FlaskConnectFactory} from 'models/FlaskModelFactory';
+import Map from 'can/map/';
+
+export let Task = {
+  connection: FlaskConnectFactory({
+    url: '/api/tasks_task',
+    name: 'tasks_task',
+    map: Map.extend({
+      name: 'test',
+      description: 'test',
+      yes_or_no: false
+    })
+  }),
+  title: 'Tasks',
+  id: 'tasks'
+};
