4f8fda86a5d73ef9ed68d955e29426da552d89d0
roemhildtg <roemhildtg@gmail.com>
2016-03-30 09:51:38 -0500

    Use full paths to components

--- app/viewer/components.js
+++ app/viewer/components.js
@@ -11,16 +11,16 @@ import 'bootstrap';
 
 //import components used by this app
 //TODO: these may be converted into <can-import> tags eventually
-import 'components/measure-widget/';
-import 'components/ol-map/';
-import 'components/panel-container/';
-import 'components/tab-container/';
-import 'components/identify-widget/';
-import 'components/ol-popup/';
-import 'components/locator-widget/';
-import 'components/print-widget/';
-import 'components/layer-control/';
-import 'components/crud-manager/';
+import 'components/measure-widget/measure-widget';
+import 'components/ol-map/ol-map';
+import 'components/panel-container/panel-container';
+import 'components/tab-container/tab-container';
+import 'components/identify-widget/identify-widget';
+import 'components/ol-popup/ol-popup';
+import 'components/locator-widget/locator-widget';
+import 'components/print-widget/print-widget';
+import 'components/layer-control/layer-control';
+import 'components/crud-manager/crud-manager';
 import 'components/layer-control/layer-components/Default';
 import 'components/layer-control/layer-components/Group';
 import 'components/layer-control/layer-components/TileWMS';
