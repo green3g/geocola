489798b9648b279a560e0511270c83d6dd461bec
roemhildtg <roemhildtg@gmail.com>
2016-05-14 16:08:54 -0500

    Move fixtures/json test data into the test folder

--- components/form-widget/form-widget.test.js
+++ components/form-widget/form-widget.test.js
@@ -1,44 +1,13 @@
-import {
-  ViewModel
-} from './form-widget';
-import fixture from 'can/util/fixture/';
+import { ViewModel } from './form-widget';
 import q from 'steal-qunit';
-import superMap from 'can-connect/can/super-map/';
-import CanMap from 'can/map/';
-import List from 'can/list/';
-import data from './test/tasks.json';
 
-let vm, model;
+import { Connection } from 'test/data/connection';
 
-//a mock ajax service
-fixture({
-  '/tasks': function() {
-    return data;
-  },
-  '/tasks/{id}': function(params){
-    return data.filter(function(item){
-      return item.id == params.data.id;
-    })[0];
-  }
-});
+let vm;
 
 q.module('components/form-widget.ViewModel', {
   beforeEach: function() {
     vm = new ViewModel({});
-
-    let map = CanMap.extend({
-      item1: 'something',
-      item2: 'something else'
-    });
-    model = superMap({
-      idProp: 'id',
-      Map: map,
-      List: List.extend({
-        Map: map
-      }),
-      url: '/tasks',
-      name: 'task'
-    });
   },
   afterEach: function() {
     vm = null;
@@ -49,10 +18,10 @@ test('objectId set()', assert => {
   let done = assert.async();
   let id = 1;
   vm.attr({
-    connection: model,
+    connection: Connection,
     objectId: id
   });
-  vm.attr('promise').then(function(){
+  vm.attr('promise').then(function() {
     assert.equal(vm.attr('formObject.id'), id, 'formObject should be retrieved correctly');
     done();
   });
@@ -61,7 +30,7 @@ test('objectId set()', assert => {
 test('fetchObject(con, id)', assert => {
   let id = 1;
   let done = assert.async();
-  let promise = vm.fetchObject(model, id);
+  let promise = vm.fetchObject(Connection, id);
   promise.then(function(item) {
     assert.equal(vm.attr('formObject.id'), id, 'form object should be loaded asynchronously');
     done();
@@ -70,10 +39,10 @@ test('fetchObject(con, id)', assert => {
 
 test('submitForm()', assert => {
   let done = assert.async();
-  let object = {test: 'hello'};
+  let object = { test: 'hello' };
   vm.attr('formObject', object);
 
-  vm.on('submit', function(ev, item){
+  vm.on('submit', function(ev, item) {
     assert.deepEqual(item.attr(), object, 'item dispatched from submit event should be the object');
     done();
   });
@@ -81,7 +50,7 @@ test('submitForm()', assert => {
 });
 
 test('setField(field, domElement, event, value)', assert => {
-  let object = {test: 'hello'};
+  let object = { test: 'hello' };
   vm.attr('formObject', object);
 
   vm.setField('test', null, null, 'dummy');
@@ -91,7 +60,7 @@ test('setField(field, domElement, event, value)', assert => {
 test('cancelForm()', assert => {
   let done = assert.async();
 
-  vm.on('cancel', function(){
+  vm.on('cancel', function() {
     assert.equal(1, 1, 'cancel event should be dispatched');
     done();
   });
