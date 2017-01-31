0ed3e729a14b1fa03ed96bb52658826d97e136fe
Gregg Roemhildt <roemhildtg@gmail.com>
2017-01-31 16:09:57 -0600

    update deps

--- build.js
+++ build.js
@@ -1,27 +1,9 @@
+var stealTools = require('steal-tools');
+var path = require('path');
 
-var stealTools = require("steal-tools");
-
-stealTools.export({
-	system: {
-		config: __dirname+"/package.json!npm"
-	},
-	outputs: {
-		"+amd": {
-			ignore: false
-		},
-		"+cjs": {
-			ignore: false
-		},
-		"+global-js": {
-			modules: ['app/viewer/viewer'],
-			ignore: false
-		},
-		'+global-css': {}
-	}
-}).catch(function(e){
-
-	setTimeout(function(){
-		throw e;
-	},1);
-
+stealTools.build({
+    config: path.join(__dirname, 'package.json!npm')
+}, {
+    bundleAssets: true,
+    sourceMaps: true
 });
